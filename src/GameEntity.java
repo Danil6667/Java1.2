3d46839773bd2fa5725368f8431a7ba230a65558
Данил <gnilidanil@gmail.com>
2022-09-08 22:37:59 +0600

    Initial commit

---
+++ src/GameEntity.java
@@ -0,0 +1,24 @@
+public class GameEntity {
+    private int hp;
+    private int damage;
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    public String info(){
+        return (getHp() + " " + getDamage());
+    }
+}
+
---
+++ src/Sceleton.java
@@ -0,0 +1,18 @@
+
+    public class Sceleton extends Boss{
+        private int numberOfArrows;
+
+        public int getNumberOfArrows() {
+            return numberOfArrows;
+
+        }
+
+        public void setNumberOfArrows(int numberOfArrows) {
+            this.numberOfArrows = numberOfArrows;
+        }
+
+        public String ptintInfo() {
+            return super.info() + " " + getNumberOfArrows();
+        }
+    }
+
